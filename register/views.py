94f67ffe3cac8bb6ad9febe023ed657a88baaa11
MartinsMessias <messiasads2017@gmail.com>
2021-05-22 14:17:41 -0500

    Added registration function

--- register/views.py
+++ register/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterForm
 
 
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
         return redirect('/')
 
-    form = UserCreationForm()
+    form = RegisterForm()
     return render(request, 'register/register.html', {'form': form})
